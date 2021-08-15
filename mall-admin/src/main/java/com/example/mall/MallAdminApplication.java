e9735a57fa66f95e736d60040630ef9635f79487
giteeUser <2855971203@qq.com>
2021-08-15 21:42:19 +0800

    mbgå开发

---
+++ mall-admin/src/main/java/com/example/mall/MallAdminApplication.java
@@ -0,0 +1,18 @@
+package com.example.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName MallAdminApplication
+ * @Description 应用启动入口
+ * @Author wangHao
+ * @DATE 2021/8/13 20:53
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class MallAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallAdminApplication.class, args);
+    }
+}
